6cc8fa554f92361b48906fe48c6f731c29eed853
John-Fumo <fumo@purdue.edu>
2023-11-07 16:49:25 -0500

    Added test module

---
+++ Test_Module/src/testModule.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class testModule {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String in = "";
+        in = scanner.nextLine();
+        System.out.println(in);
+    }
+
+}
