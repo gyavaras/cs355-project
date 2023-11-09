18532b5bf10451284473d812879e674e2bebb1ee
John-Fumo <fumo@purdue.edu>
2023-11-09 16:54:04 -0500

    Added server socket

---
+++ Final_Project/src/Server.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable{
+
+    @Override
+    public void run() {
+        ServerSocket serverSocket = null;
+    }
+}
