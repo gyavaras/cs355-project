6f3b67fbfecd45baa2809d44ade64138c09dcf52
brennanhorn1 <89666167+brennanhorn1@users.noreply.github.com>
2023-11-14 17:00:59 -0500

    Update

--- Final_Project/src/Client.java
+++ Final_Project/src/Client.java
@@ -1,46 +1,96 @@
-import java.io.*;
-import java.net.*;
-import java.util.*;
-public class Client {
-    // driver code
-    public static void main(String[] args)
-    {
-        // establish a connection by providing host and port
-        // number
-        try (Socket socket = new Socket("localhost", 1234)) {
+import javax.crypto.Cipher;
+import javax.crypto.Mac;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.Arrays;
 
-            // writing to server
-            PrintWriter out = new PrintWriter(
-                    socket.getOutputStream(), true);
+public class Client implements Runnable {
+    private static final String ENCRYPTION_ALGORITHM = "AES/CBC/PKCS5Padding";
+    private static final String HASH_ALGORITHM = "HmacSHA256";
+    private static final int IV_SIZE = 16; // AES block size
+    private static final SecureRandom secureRandom = new SecureRandom();
 
-            // reading from server
-            BufferedReader in
-                    = new BufferedReader(new InputStreamReader(
-                    socket.getInputStream()));
+    private final Server server;
+    private final String codeSegment;
+    private final byte[] encryptionKey;
+    private final byte[] macKey;
 
-            // object of scanner class
-            Scanner sc = new Scanner(System.in);
-            String line = null;
+    public Client(Server server, String codeSegment, byte[] encryptionKey, byte[] macKey) {
+        this.server = server;
+        this.codeSegment = codeSegment;
+        this.encryptionKey = encryptionKey;
+        this.macKey = macKey;
+    }
 
-            while (!"exit".equalsIgnoreCase(line)) {
-
-                // reading from user
-                line = sc.nextLine();
-
-                // sending the user input to server
-                out.println(line);
-                out.flush();
-
-                // displaying server reply
-                System.out.println("Server replied "
-                        + in.readLine());
-            }
-
-            // closing the scanner object
-            sc.close();
-        }
-        catch (IOException e) {
+    @Override
+    public void run() {
+        try {
+            EncryptedData data = prepareDataForServer();
+            server.receiveDataFromClient(this, data);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    private EncryptedData prepareDataForServer() throws Exception {
+        byte[] hash = hash(codeSegment);
+        IvParameterSpec iv = generateIv();
+        byte[] encrypted = encrypt(encryptionKey, iv.getIV(), hash);
+        byte[] hmac = createHmac(macKey, encrypted);
+        return new EncryptedData(encrypted, hmac, iv);
+    }
+
+    private byte[] hash(String data) throws Exception {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        return digest.digest(data.getBytes("UTF-8"));
+    }
+
+    private IvParameterSpec generateIv() {
+        byte[] iv = new byte[IV_SIZE];
+        secureRandom.nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    private byte[] encrypt(byte[] key, byte[] iv, byte[] plaintext) throws Exception {
+        Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+        return cipher.doFinal(plaintext);
+    }
+
+    private byte[] createHmac(byte[] key, byte[] data) throws Exception {
+        Mac hmac = Mac.getInstance(HASH_ALGORITHM);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, HASH_ALGORITHM);
+        hmac.init(secretKeySpec);
+        return hmac.doFinal(data);
+    }
+
+    // Inner class for encapsulating encrypted data
+    public static class EncryptedData {
+        private final byte[] encryptedData;
+        private final byte[] hmac;
+        private final IvParameterSpec iv;
+
+        public EncryptedData(byte[] encryptedData, byte[] hmac, IvParameterSpec iv) {
+            this.encryptedData = encryptedData;
+            this.hmac = hmac;
+            this.iv = iv;
+        }
+
+        public byte[] getEncryptedData() {
+            return encryptedData;
+        }
+
+        public byte[] getHmac() {
+            return hmac;
+        }
+
+        public IvParameterSpec getIv() {
+            return iv;
+        }
+    }
 }
+
--- Final_Project/src/Server.java
+++ Final_Project/src/Server.java
@@ -1,41 +1,128 @@
-import java.io.IOException;
-import java.io.*;
-import java.net.*;
+import java.util.concurrent.CountDownLatch;
+import javax.crypto.Cipher;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+import java.util.Arrays;
 
 public class Server {
-    public static void main(String[] args) {
-        //define the server socket
-        ServerSocket server = null;
-        try {
-            //server is now listening on port 8008
-            server = new ServerSocket(8008);
-            server.setReuseAddress(true);
+    private final byte[] encryptionKey; // Key used for decryption
+    private final byte[] macKey; // Key used for HMAC verification
+    private Client.EncryptedData aliceData; // Encrypted data received from Alice
+    private Client.EncryptedData bobData; // Encrypted data received from Bob
+    private final CountDownLatch latch; // Synchronizer to wait for both clients
 
-            //start the infinite loop for client requests
-            while(true) {
-                //make a client socket to accept incoming clients
-                Socket client = server.accept();
-                // Displaying that new client is connected to server
-                System.out.println("New client connected" + client.getInetAddress().getHostAddress());
-                // create a new thread object
-                ClientHandler clientSock = new ClientHandler(client);
+    /**
+     * Constructor for the Server class.
+     *
+     * @param encryptionKey The AES encryption key.
+     * @param macKey        The HMAC key.
+     * @param clientCount   The number of clients (should be 2 for Alice and Bob).
+     */
+    public Server(byte[] encryptionKey, byte[] macKey, int clientCount) {
+        this.encryptionKey = encryptionKey;
+        this.macKey = macKey;
+        this.latch = new CountDownLatch(clientCount);
+    }
 
-                // This thread will handle the client
-                // separately
-                new Thread(clientSock).start();
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (server != null) {
-                try {
-                    server.close();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    /**
+     * Receives encrypted data from a client.
+     * This method is synchronized to handle concurrent access.
+     *
+     * @param client The client sending the data (either Alice or Bob).
+     * @param data   The encrypted data from the client.
+     */
+    public synchronized void receiveDataFromClient(Client client, Client.EncryptedData data) {
+        if (client instanceof Alice) {
+            aliceData = data;
+        } else if (client instanceof Bob) {
+            bobData = data;
         }
+        latch.countDown();
+    }
+
+    /**
+     * Starts the comparison of data from both clients.
+     * This method waits until both clients have sent their data.
+     *
+     * @throws InterruptedException if the current thread is interrupted while waiting.
+     */
+    public void startComparison() throws InterruptedException {
+        latch.await(); // Wait for both clients to send their data
+        compare(aliceData, bobData);
+    }
+
+    /**
+     * Compares the encrypted data received from both clients.
+     *
+     * @param data1 Encrypted data from the first client.
+     * @param data2 Encrypted data from the second client.
+     */
+    private void compare(Client.EncryptedData data1, Client.EncryptedData data2) {
+        try {
+            byte[] decryptedData1 = decryptAndVerifyHMAC(data1);
+            byte[] decryptedData2 = decryptAndVerifyHMAC(data2);
+
+            if (decryptedData1 != null && decryptedData2 != null) {
+                if (Arrays.equals(decryptedData1, decryptedData2)) {
+                    System.out.println("The server determined that the two clients have the same code segment.");
+                } else {
+                    System.out.println("The server determined that the two clients do not have the same code segment.");
+                }
+            } else {
+                System.out.println("HMAC verification failed. Cannot compare the data.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Decrypts the data and verifies the HMAC.
+     *
+     * @param data The encrypted data with HMAC.
+     * @return Decrypted data if HMAC is verified, null otherwise.
+     * @throws Exception if an error occurs during decryption or HMAC verification.
+     */
+    private byte[] decryptAndVerifyHMAC(Client.EncryptedData data) throws Exception {
+        if (verifyHmac(data.getHmac(), macKey, data.getEncryptedData())) {
+            return decrypt(encryptionKey, data.getIv().getIV(), data.getEncryptedData());
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Verifies the HMAC of the given data.
+     *
+     * @param hmac The HMAC to be verified.
+     * @param key  The key used for HMAC generation.
+     * @param data The original data.
+     * @return true if the HMAC is valid, false otherwise.
+     * @throws Exception if an error occurs during HMAC verification.
+     */
+    private boolean verifyHmac(byte[] hmac, byte[] key, byte[] data) throws Exception {
+        Mac hmacInstance = Mac.getInstance(Client.HASH_ALGORITHM);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, Client.HASH_ALGORITHM);
+        hmacInstance.init(secretKeySpec);
+        byte[] expectedHmac = hmacInstance.doFinal(data);
+        return Arrays.equals(expectedHmac, hmac);
+    }
+
+    /**
+     * Decrypts the given ciphertext using AES/CBC/PKCS5Padding.
+     *
+     * @param key        The AES key.
+     * @param iv         The initialization vector.
+     * @param ciphertext The ciphertext to be decrypted.
+     * @return The decrypted plaintext.
+     * @throws Exception if an error occurs during decryption.
+     */
+    private byte[] decrypt(byte[] key, byte[] iv, byte[] ciphertext) throws Exception {
+        Cipher cipher = Cipher.getInstance(Client.ENCRYPTION_ALGORITHM);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+        return cipher.doFinal(ciphertext);
     }
 }
