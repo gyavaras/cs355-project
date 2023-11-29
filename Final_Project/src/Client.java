5b429014901af7fcd3b610e2bd699507efab67d0
John-Fumo <fumo@purdue.edu>
2023-11-29 10:37:40 -0500

    Comments

--- Final_Project/src/Client.java
+++ Final_Project/src/Client.java
@@ -22,31 +22,36 @@ import java.security.DigestInputStream; // If you're using DigestInputStream for
 import java.security.MessageDigest;
 
 public class Client implements Runnable, Serializable {
+    // Constants for encryption and hashing algorithms
     public static final String ENCRYPTION_ALGORITHM = "AES/CBC/PKCS5Padding";
     public static final String HASH_ALGORITHM = "HmacSHA256";
     private static final int IV_SIZE = 16;
     private static final SecureRandom secureRandom = new SecureRandom();
-
+    // Client-specific information
     private final String clientId;
     private final List<String> codeSegmentList = new ArrayList<>();
     private final byte[] encryptionKey;
     private final byte[] macKey;
     private transient Socket socket;
-
+    // Constructor to initialize client with ID, file paths, and keys
     public Client(String clientId, List<String> filePaths, byte[] encryptionKey, byte[] macKey) throws IOException {
         this.clientId = clientId;
         this.encryptionKey = encryptionKey;
         this.macKey = macKey;
+        // Hash code segments from provided file paths
         for (String filePath : filePaths) {
             this.codeSegmentList.add(hashFile(filePath));
         }
+        // Connect to the server
         this.socket = new Socket("localhost", 12345); // Connect to server
     }
 
+    // Implementation of the Runnable interface for concurrent execution
     @Override
     public void run() {
         try (ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
             out.writeObject(clientId); // Send the client ID first
+            // Send encrypted data for each code segment to the server
             for (String codeSegment : codeSegmentList) {
                 EncryptedData data = prepareDataForServer(codeSegment);
                 out.writeObject(data); // Send data to server
@@ -62,7 +67,7 @@ public class Client implements Runnable, Serializable {
             }
         }
     }
-
+    // Method to hash the contents of a file using SHA-256
     private String hashFile(String filePath) {
         try (InputStream fis = new BufferedInputStream(new FileInputStream(filePath));
              DigestInputStream dis = new DigestInputStream(fis, MessageDigest.getInstance("SHA-256"))) {
@@ -76,7 +81,7 @@ public class Client implements Runnable, Serializable {
             return null;
         }
     }
-
+    // Utility method to convert byte array to hexadecimal string
     private static String bytesToHex(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (byte b : bytes) {
@@ -84,7 +89,7 @@ public class Client implements Runnable, Serializable {
         }
         return sb.toString();
     }
-
+    // Method to prepare encrypted data for transmission to the server
     private EncryptedData prepareDataForServer(String data) throws Exception {
         byte[] hash = hash(data);
         IvParameterSpec iv = generateIv();
@@ -93,18 +98,18 @@ public class Client implements Runnable, Serializable {
         return new EncryptedData(encrypted, hmac, iv.getIV());  // Pass IV as byte array
     }
 
-
+    // Method to hash data using SHA-256
     private byte[] hash(String data) throws Exception {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         return digest.digest(data.getBytes("UTF-8"));
     }
-
+    // Method to generate a random Initialization Vector (IV)
     private IvParameterSpec generateIv() {
         byte[] iv = new byte[IV_SIZE];
         secureRandom.nextBytes(iv);
         return new IvParameterSpec(iv);
     }
-
+    // Method to encrypt data using AES algorithm
     private byte[] encrypt(byte[] key, byte[] iv, byte[] plaintext) throws Exception {
         Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM);
         SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
@@ -112,7 +117,7 @@ public class Client implements Runnable, Serializable {
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
         return cipher.doFinal(plaintext);
     }
-
+    // Method to create HMAC (Hash-based Message Authentication Code)
     private byte[] createHmac(byte[] key, byte[] data) throws Exception {
         Mac hmac = Mac.getInstance(HASH_ALGORITHM);
         SecretKeySpec secretKeySpec = new SecretKeySpec(key, HASH_ALGORITHM);
